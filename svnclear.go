532feb999999c37d3c58e49a42471811d7d64603
itang <live.tang@gmail.com>
2012-11-11 20:55:34 +0800

    cleanup

--- svnclear.go
+++ svnclear.go
@@ -9,32 +9,20 @@ import (
 
 var (
 	wd, _     = os.Getwd()
-	targetDir = flag.String("dir", wd, "svn root dir")
+	targetDir = flag.String("dir", wd, "The root dir")
 	count     = 0
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 	filepath.Walk(*targetDir, func(path string, info os.FileInfo, err error) error {
-		//fmt.Println(path, info.Name())
-		if info.IsDir() && info.Name() == ".svn" /*&& exists(path)*/ {
-			fmt.Printf("\"%v\" is .svn dir, try to delete it...\n", path)
+		if info.IsDir() && info.Name() == ".svn" {
+			fmt.Printf(">try to delete \"%v\"\n", path)
 			os.RemoveAll(path)
 			count++
-			fmt.Printf("delete \"%v\"!\n", path)
 			return filepath.SkipDir
 		}
 		return nil
 	})
-	fmt.Printf("delete %v .svn dir.\n", count)
-}
-
-// Exists reports whether the named file or directory exists.
-func exists(name string) bool {
-	if _, err := os.Stat(name); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
+	fmt.Printf("summary: delete %v .svn dir.\n", count)
 }
